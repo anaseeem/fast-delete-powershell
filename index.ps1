f5917cdc712afcf0fcd368d146d6dc64c1c22d35
anaseeem <ashfaqnaseem1@gmail.com>
2025-12-16 05:58:44 +0600

    initial commit

---
+++ index.ps1
@@ -0,0 +1,13 @@
+param (
+    [Parameter(ValueFromRemainingArguments = $true)]
+    [string[]]$Paths
+)
+
+$QuotedPaths = $Paths | ForEach-Object { "`"$_`"" }
+$PathArguments = $QuotedPaths -join ' '
+
+Start-Process -FilePath "pwsh.exe" `
+    -ArgumentList "-ExecutionPolicy Bypass -File `".\src\command.ps1`" $PathArguments" `
+    -NoNewWindow
+
+
