e56b4b1598cc439cd4992536240f372778ba7425
anaseeem <ashfaqnaseem1@gmail.com>
2025-12-16 06:24:42 +0600

    bug fix

--- src/command.ps1
+++ src/command.ps1
@@ -2,11 +2,6 @@ param (
     [Parameter(ValueFromRemainingArguments = $true)]
     [string[]]$Paths
 )
-foreach ($P in $Paths) {
-    Write-Host "Outside $P"
-}
-Read-Host "Press Enter to close the test window."
-return
 $TimeTaken = Measure-Command {
     foreach ($CurrentPath in $Paths) {
         Write-Host $CurrentPath
@@ -14,15 +9,14 @@ $TimeTaken = Measure-Command {
         if ([string]::IsNullOrWhiteSpace($CurrentPath)) { return }
 
         if (Test-Path $CurrentPath -PathType Container) {
-            # if this is a directory like node_modules
             Write-Host "Listing contents of: $CurrentPath"
 
             Get-ChildItem -LiteralPath $CurrentPath -Force | ForEach-Object -Parallel {
 
                 $ItemToDelete = $_
 
-                Write-Host "Parallel Deleting: $($ItemToDelete.FullName)"
-                # Remove-Item -LiteralPath $ItemToDelete -Force -Recurse -ErrorAction SilentlyContinue
+                # Write-Host "Parallel Deleting: $($ItemToDelete.FullName)"
+                Remove-Item -LiteralPath $ItemToDelete -Force -Recurse -ErrorAction SilentlyContinue
 
 
             } -ThrottleLimit 4
@@ -30,8 +24,8 @@ $TimeTaken = Measure-Command {
         }
 
         elseif (Test-Path $CurrentPath) {
-            Write-Host "Deleting: $CurrentPath" 
-            # Remove-Item -LiteralPath $CurrentPath -Force -Recurse -ErrorAction SilentlyContinue
+            # Write-Host "Deleting: $CurrentPath" 
+            Remove-Item -LiteralPath $CurrentPath -Force -Recurse -ErrorAction SilentlyContinue
         }
     }
 }
@@ -40,7 +34,6 @@ $TimeTaken = Measure-Command {
 Write-Host "---"
 Write-Host "Using powershell : $($PSVersionTable.PSVersion)"
 Write-Host "✅ Process Complete!"
-Write-Host "Total time taken: $($TimeTaken.ToString()) seconds"
 Write-Host "Total time taken in seconds: $($TimeTaken.TotalSeconds) seconds"
 
 Read-Host "Press Enter to close the test window."
